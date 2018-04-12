7901b341d0f27ee9f74dcbb3d24c7c0dfa7a4edb
ShiinaOrez <shiina_orez@qq.com>
2018-04-12 20:39:18 +0800

    xxx

---
+++ Go程序设计语言/1/1-7.go
@@ -0,0 +1,25 @@
+package main
+
+import(
+	"fmt"
+	_"io/ioutil"
+	"net/http"
+	"io"
+	"os"
+)
+
+func main(){
+	for _,url:=range os.Args[1:]{
+		resp,err:=http.Get(url)
+		if(err!=nil){
+			fmt.Fprintf(os.Stderr,"fetch:%v\n",err)
+			os.Exit(1)
+		}
+		n,err:=io.Copy(os.Stdout,resp.Body)
+		resp.Body.Close()
+		if(err!=nil){
+			fmt.Fprintf(os.Stderr,"fetch:reading %s: %v\n",url,err)
+		}
+		fmt.Printf("%d\n",n)
+	}
+}
---
+++ Go程序设计语言/1/1-9.go
@@ -0,0 +1,35 @@
+package main
+
+import(
+	"fmt"
+	_"io/ioutil"
+	"net/http"
+	"io"
+	"os"
+	"strings"
+)
+
+const (
+    prefix="https://"
+)
+
+func main(){
+	for _,url:=range os.Args[1:]{
+		if(!strings.HasPrefix(url,prefix)){
+			url=prefix+url
+		}
+		resp,err:=http.Get(url)
+		if(err!=nil){
+			fmt.Fprintf(os.Stderr,"fetch:%v\n",err)
+			os.Exit(1)
+		}
+		n,err:=io.Copy(os.Stdout,resp.Body)
+		resp.Body.Close()
+		if(err!=nil){
+			fmt.Fprintf(os.Stderr,"fetch:reading %s: %v\n",url,err)
+		}
+		fmt.Printf("response length=%d %s\n",n,resp.Status)
+	}
+}
+
+
